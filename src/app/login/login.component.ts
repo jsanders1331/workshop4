b7b24ff711930bcacdb771443f707dda7cc3e4c4
james.sanders1331 <james.sanders1337@gmail.com>
2022-08-11 11:00:49 +1000

    wk4

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userName = ""
+  password = ""
+  userlist = [{"userName": "123", "password": "123"}]
+  constructor(private router:Router) { }
+
+  check(){
+    let found = this.userlist.some(e=>(e.userName == this.userName && e.password == this.password));
+    if(found){
+      alert("valid")
+      this.router.navigate(['/','account'])
+    } else{
+      alert("invalid user")
+    }
+  }
+  ngOnInit(): void {
+  }
+
+}
