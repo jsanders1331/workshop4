007744b6070207698e7acc22866efe69dd2130db
james.sanders1337@gmail.com <james.sanders1337@gmail.com>
2022-08-29 01:52:13 +1000

    feat: server client auth hard coded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AccountComponent } from "./account/account.component"
+import { AccountComponent } from './account/account.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [{path: "login", component:LoginComponent},{path: "account", component:AccountComponent}];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { findIndex } from 'rxjs';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,28 @@ import { findIndex } from 'rxjs';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  username = 'John';
+  username = 'John1';
   password = '123';
   fetchedData = {};
+  authenticated = false;
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  loggedIn() {
+    if (sessionStorage.getItem('user_data') == null) {
+      return false;
+    }
+    console.log(sessionStorage.getItem('user_data'));
+    var data = sessionStorage.getItem('user_data') || '{}';
+    var data_JSON = JSON.parse(data);
+    return data_JSON[0].valid;
+  }
+
+  cardClasses() {
+    return {
+      hidemessage: this.authenticated,
+      showmessage: !this.authenticated,
+    };
+  }
 
   submit() {
     fetch('http://localhost:3000/api/auth', {
@@ -28,16 +48,24 @@ export class LoginComponent implements OnInit {
           .then((data) => {
             if (data[0].valid) {
               console.log('Authenticated');
+              this.authenticated = true;
               sessionStorage.setItem(
                 'user_data',
                 JSON.stringify(this.fetchedData)
               );
               console.log('hello', this.fetchedData);
+              this.loggedIn();
+              this.router.navigate(['/profile']);
+              return true;
+            } else {
+              this.authenticated = false;
+              return false;
             }
           })
       );
 
     // https://caniuse.com/?search=fetch f*k IE users
   }
+
   ngOnInit(): void {}
 }
